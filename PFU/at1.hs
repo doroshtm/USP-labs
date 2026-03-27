852e876dedce15a3e6cc30f055470b442fb8e702
Gabriel Carraro Salzedas <doroshtm>
2026-03-27 19:56:32 -0300

    +haskell +algumas coisas antigas

---
+++ PFU/at1.hs
@@ -0,0 +1,20 @@
+import Distribution.Simple.Setup (falseArg)
+main :: IO ()
+main = do
+    a <- readLn
+    b <- readLn
+    c <- readLn
+    putStrLn $ heron a b c
+
+
+ehTriangulo a b c
+    | a>b+c = False
+    | b>a+c = False
+    | c>a+b = False
+    | otherwise = True
+heron a b c
+    | ehTriangulo a b c = show area
+    | otherwise = "-"
+    where
+        s = (a+b+c)/2
+        area = sqrt(s*(s-a)*(s-b)*(s-c))
